496e34dc2a5ebcd0125d068bb1d6919f10fae04b
flavglen <flavglen@gmail.com>
2021-08-01 14:33:23 -0400

    Updates

---
+++ precache-manifest.014a9cbeb00632bd7bd382c81b9663e2.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "306371197637454b6b43d0ce0a083875",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dec3fe668d373748de06",
+    "url": "/static/css/main.5748b904.chunk.css"
+  },
+  {
+    "revision": "c42df4e29b5458816730",
+    "url": "/static/js/2.4acfc44c.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/static/js/2.4acfc44c.chunk.js.LICENSE"
+  },
+  {
+    "revision": "41b198afa66462a89cb7",
+    "url": "/static/js/3.8504a88b.chunk.js"
+  },
+  {
+    "revision": "048b7bd224aca27dc55bd984098d22be",
+    "url": "/static/js/3.8504a88b.chunk.js.LICENSE"
+  },
+  {
+    "revision": "076967054b924d906ffc",
+    "url": "/static/js/4.8ce15ed0.chunk.js"
+  },
+  {
+    "revision": "129768dbb291f7de654a",
+    "url": "/static/js/5.6ea8dc1c.chunk.js"
+  },
+  {
+    "revision": "dec3fe668d373748de06",
+    "url": "/static/js/main.dd15fdf2.chunk.js"
+  },
+  {
+    "revision": "449eeca1e5ce99121d73",
+    "url": "/static/js/runtime-main.ca369266.js"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/static/media/logo.ee7cd8ed.svg"
+  }
+]);
